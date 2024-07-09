de14c58a09676c13044ded842d0c03fe0c1cbc42
Brennan Spies <brennanspies@gmail.com>
2024-07-08 18:54:07 -0700

    Move package for BuilderTests

--- core/src/test/java/org/workhorse/builder/BuilderTests.java
+++ core/src/test/java/org/workhorse/builder/BuilderTests.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.workhorse.test.builder;
+package org.workhorse.builder;
 
 import org.junit.Test;
 import org.workhorse.actor.Actor;
